3c32794197a05f36934901158c5a11a7209f3a20
B3li4s <b3li4s@yahoo.fr>
2014-03-21 12:57:44 +0100

    format

--- src/main/java/fr/univaix/iut/pokebattle/smartcell/PokemonCriesCell.java
+++ src/main/java/fr/univaix/iut/pokebattle/smartcell/PokemonCriesCell.java
@@ -6,15 +6,14 @@ import fr.univaix.iut.pokebattle.twitter.Tweet;
  * Reply to all.
  */
 public class PokemonCriesCell implements SmartCell {
-	
-	public PokemonCriesCell () {
-	}
-	
-    public String ask(Tweet question) {
-        if (question.getScreenName() != null) {
-            return "@" + question.getScreenName() + " Pika pika";
-        }
-        return "Pika pika";
-    }
 
+	public PokemonCriesCell() {
+	}
+
+	public String ask(Tweet question) {
+		if (question.getScreenName() != null) {
+			return "@" + question.getScreenName() + " Pika pika";
+		}
+		return "Pika pika";
+	}
 }
