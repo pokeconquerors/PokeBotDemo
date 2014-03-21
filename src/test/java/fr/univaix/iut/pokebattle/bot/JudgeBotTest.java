23a9acea93a71cc71a4545a9ff88671c5d91370c
B3li4s <b3li4s@yahoo.fr>
2014-03-21 14:23:04 +0100

    Ajout de tests
    Ajouts pour tester les différentes réponses du bot

--- src/test/java/fr/univaix/iut/pokebattle/bot/JudgeBotTest.java
+++ src/test/java/fr/univaix/iut/pokebattle/bot/JudgeBotTest.java
@@ -11,7 +11,21 @@ public class JudgeBotTest {
     @Test
     public void testAsk() throws Exception {
         assertThat(judgeBot.ask(new Tweet("Salut"))).isNull();
+    }
+    
+    @Test
+    public void testNotImplementedNoAnswer() throws Exception {
         assertThat(judgeBot.ask(new Tweet("This is not a question."))).isNull();
+    }
+    
+    @Test
+    public void testNoOwnerNoSalut() {
+    	assertThat(judgeBot.ask(new Tweet("Salut!"))).isNull();
+    }
+    
+    @Test
+    public void testTweetHadOwnerHaveAnswer_Salut() {
+    	assertThat(judgeBot.ask(new Tweet("TwitterTest","Salut!"))).isNotNull();
     }
 
    
